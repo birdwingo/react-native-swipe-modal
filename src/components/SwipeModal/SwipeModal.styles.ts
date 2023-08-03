44e13ff5b4b2c3dbd872e68ff2fd368e859d209f
LukasFridmansky <luky.fridmansky@gmail.com>
2023-08-03 15:52:38 +0200

    feat: swipe modal bar

---
+++ src/components/SwipeModal/SwipeModal.styles.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create( {
+  barContainer: {
+    width: '100%',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bar: {
+    width: 34,
+    height: 5,
+    borderRadius: 10,
+  },
+} );
