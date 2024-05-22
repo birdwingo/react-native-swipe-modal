4b56fb9e6b3ae5156e9cb31a8c11b2ac00ad9cb3
Nicholas Barrow <me@nicholasrbarrow.com>
2024-05-22 12:44:54 -0400

    feat: controlled state test

--- src/core/dto/animatedModalDTO.ts
+++ src/core/dto/animatedModalDTO.ts
@@ -1,8 +1,11 @@
 import { ReactNode } from 'react';
 
-export interface AnimatedModalProps {
-  children: ReactNode | ReactNode[];
+export type AnimatedModalBaseProps = {
+  controlled?: boolean;
+  open?: boolean;
+  setOpen?: ( value: boolean ) => void;
   visible?: boolean;
+  children: ReactNode | ReactNode[];
   onShow?: () => void;
   onHide?: () => void;
   closeOnEmptySpace?: boolean;
@@ -11,7 +14,23 @@ export interface AnimatedModalProps {
   closeSpaceVisibility?: number;
   hideKeyboardOnShow?: boolean;
   useKeyboardAvoidingView?: boolean;
-}
+};
+
+export type ControlledAnimatedModalProps = AnimatedModalBaseProps & {
+  visible?: never;
+  controlled: true;
+  open: boolean;
+  setOpen: ( value: boolean ) => void;
+};
+
+export type ForwardRefAnimatedModalProps = AnimatedModalBaseProps & {
+  visible?: boolean;
+  controlled?: never;
+  open?: never;
+  setOpen?: never;
+};
+
+export type AnimatedModalProps = ControlledAnimatedModalProps | ForwardRefAnimatedModalProps;
 
 export type AnimatedModalPublicMethods = {
   show: () => void;
